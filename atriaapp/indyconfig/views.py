138a2a39aceb2c704a8bb429dac4e1a8eb05c45b
Ian Costanzo <ian@anon-solutions.ca>
2019-02-12 09:17:17 -0800

    Updated invitation format expected by connect.me

--- atriaapp/indyconfig/views.py
+++ atriaapp/indyconfig/views.py
@@ -934,8 +934,53 @@ def list_wallet_credentials(request):
 ###########################################
 def get_invitation_text(request, token):
     connection = VcxConnection.objects.filter(token=token).first()
-    invitation = json.loads(connection.invitation)
-    return JsonResponse(invitation)
+
+    # find owner of the wallet that issued this invitation
+    wallet = connection.wallet_name
+    related_user = User.objects.filter(wallet_name=wallet.wallet_name).all()
+    related_org = AtriaOrganization.objects.filter(wallet_name=wallet.wallet_name).all()
+    if len(related_user) == 0 and len(related_org) == 0:
+        raise Exception('Error wallet with no owner {}'.format(wallet.wallet_name))
+
+    # TODO for now, fill in proper values later
+    if 0 < len(related_user):
+        institution_name = related_user[0].email
+        name = related_user[0].email
+        institution_logo_url = 'http://some.user/logo.url'
+    elif 0 < len(related_org):
+        institution_name = related_org[0].org_name
+        name = related_org[0].org_name
+        institution_logo_url = 'http://some.org/logo.url'
+
+    cm_invitation = convertInvite(json.loads(connection.invitation), institution_name, name, institution_logo_url)
+    
+    return JsonResponse(cm_invitation)
+
+# script from @burdettadam, map to the invite format expected by Connect.Me
+def convertInvite(invite, institution_name, name, institution_logo_url):
+    cm_invite = { "id": invite["connReqId"],
+            "s" :{"d" :invite["senderDetail"]["DID"],
+                    "dp":{"d":invite["senderDetail"]["agentKeyDlgProof"]["agentDID"],
+                          "k":invite["senderDetail"]["agentKeyDlgProof"]["agentDelegatedKey"],
+                          "s":invite["senderDetail"]["agentKeyDlgProof"]["signature"]
+                        },
+                    "l" :invite["senderDetail"]["logoUrl"],
+                    "n" :invite["senderDetail"]["name"],
+                    "v" :invite["senderDetail"]["verKey"]
+                    },
+            "sa":{"d":invite["senderAgencyDetail"]["DID"],
+                    "e":invite["senderAgencyDetail"]["endpoint"],
+                    "v":invite["senderAgencyDetail"]["verKey"]
+                },
+            "sc":invite["statusCode"],
+            "sm":invite["statusMsg"],
+            "t" :invite["targetName"]
+            }
+    cm_invite["s"]["n"] = institution_name
+    cm_invite["t"]      = name
+    cm_invite["s"]["l"] = institution_logo_url
+    return cm_invite
+
 
 class VcxConnectionView(APIView):
     authentication_classes = (IndyRestAuthentication,)
